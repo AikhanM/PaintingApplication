cbabec2f4ad839a595997093529b4a7e6b1bafb0
AikhanM <aikhanmirzaevi@gmail.com>
2023-08-18 17:14:48 +0400

    Fixed Comments

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import View from './view.js'
 import Model from './model.js'
 import Controller from './controller.js'
 
-const view = new View();
-const model = new Model();
-const controller = new Controller(view, model);
+window.addEventListener("DOMContentLoaded",()=>{
+    const view=new View()
+    const model=new Model()
+    const controller=new Controller(view,model)
+    controller.init()
+})
--- src/model.js
+++ src/model.js
@@ -1,10 +1,12 @@
-export default class model {
+export default class Model {
     constructor() {
         this.isPainting = false;
         this.prevMouseX = 0;
         this.prevMouseY = 0;
         this.color = "#000";
+        this.prevColor="#000"
         this.lineWidth = 5;
+   
     }
 
     startPath(e, ctx) {
@@ -12,40 +14,41 @@ export default class model {
             this.isPainting = true;
             this.prevMouseX = e.offsetX;
             this.prevMouseY = e.offsetY;
-            ctx.beginPath();
+            //ctx.beginPath();
         }
     }
 
     stopPath(ctx) {
         if (this.isPainting) {
             this.isPainting = false;
-            ctx.closePath();
+            //ctx.closePath();
         }
     }
 
     setColor(color) {
-        this.prevColor=color
         this.color = color;
     }
+    
+   
 
     useEraser(color) {
-     this.color=color
+        this.prevColor = this.color; 
+        this.color = color;
     }
-
-
-   
 
     drawLine(e, ctx) {
         if (this.isPainting) {
             const nextMouseX = e.offsetX;
             const nextMouseY = e.offsetY;
+            ctx.beginPath();
             ctx.lineWidth = this.lineWidth;
             ctx.strokeStyle = this.color;
             ctx.moveTo(this.prevMouseX, this.prevMouseY);
             ctx.lineTo(nextMouseX, nextMouseY);
+            ctx.lineCap = "round";
             ctx.stroke();
             ctx.closePath();
-            ctx.beginPath();
+           
             this.prevMouseX = nextMouseX;
             this.prevMouseY = nextMouseY;
         }
@@ -59,4 +62,5 @@ export default class model {
         this.lineWidth=data
         return this.lineWidth
     }
+  
 }
